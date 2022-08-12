c8a239b2d5606bb108b6c49365c98b26b46784a0
RIDHO <90787959+ridho808@users.noreply.github.com>
2022-08-12 22:26:08 +0700

    Update 04.js

--- 04.js
+++ 04.js
@@ -5,10 +5,11 @@
 const prompt = require('prompt');
 prompt.start();
 
-prompt.get('Year',(Input)=>{
+prompt.get('Year',(err,Input)=>{
     if(Input.Year % 400 == 0 || Input.Year % 4 ==0 && Input.Year % 100 !=0){
         console.log('Tahun Kabisat');
     }else{
         console.log('Bukan Tahun Kabisat')
     }
+    console.log(err)
 })
